1c0bef18d0775444074746d7c18f350b33cd37d9
Justin Dionisio <justindionisio21@yahoo.com>
2020-06-09 16:39:32 -0700

    Add middleware and array of members to use CRUD abilities

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const logger = require('./middleware/logger');
 
-const app = express()
+const app = express();
+
+// Init middleware
+// app.use(logger);
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 
 // Set static folder 
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'))
 
 const PORT = process.env.PORT || 5000;
 
