5fe0df39494e29778394317242ab8d67bb0df7ee
Justin Dionisio <justindionisio21@yahoo.com>
2020-06-10 02:40:22 -0700

    Add handlebars

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
 // Init middleware
 // app.use(logger);
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 // Body Parser Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
-// Set static folder 
+// Homepage Route
+app.get('/', (req, res) =>
+    res.render('index', {
+        title: 'Member App',
+        members
+    })
+);
+
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Members API Routes
-app.use('/api/members', require('./routes/api/members'))
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT), () => console.log(`Server started on port ${PORT}`);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
